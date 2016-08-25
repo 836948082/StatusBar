77c8495781d2217cfe6b232c842b45843e0ab4b6
836948082 <836948082@qq.com>
2016-08-25 14:45:14 +0800

    去掉导航栏透明(可能会影响用户操作)

--- app/src/main/java/com/runtai/statusbar/MainActivity.java
+++ app/src/main/java/com/runtai/statusbar/MainActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
-import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -14,14 +13,12 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewConfiguration;
-import android.view.Window;
 import android.view.WindowManager;
 
 import com.google.android.gms.appindexing.Action;
@@ -135,29 +132,26 @@ public class MainActivity extends AppCompatActivity
      * 设置状态栏颜色(沉浸式状态栏)
      */
     public void setStatusBar() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             /** 透明状态栏 */
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            /** 透明状态栏 */
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            /** 透明导航栏 */
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+//            /** 透明导航栏 */  //如果设置了这里，则导航栏覆盖在页面之上，影响页面操作
+//            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         }
-        //6.0版本(设置状态栏和导航栏在6.0系统上全透明)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            Window window = getWindow();
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
-                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(Color.TRANSPARENT);//设置状态栏在6.0系统上全透明
-//            window.setNavigationBarColor(Color.TRANSPARENT);//设置导航栏在6.0系统上全透明
-            window.setNavigationBarColor(getColor(R.color.translucent));//半透明
-            Log.e("导航栏高度", "" + getNavigationBarHeight(this));
-        }
+//        //6.0版本(设置状态栏和导航栏在6.0系统上全透明)
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            Window window = getWindow();
+//            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+//                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+//            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+//            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+//            window.setStatusBarColor(Color.TRANSPARENT);//设置状态栏在6.0系统上全透明
+////            window.setNavigationBarColor(Color.TRANSPARENT);//设置导航栏在6.0系统上全透明
+//            window.setNavigationBarColor(getColor(R.color.translucent));//半透明
+//            Log.e("导航栏高度", "" + getNavigationBarHeight(this));
+//        }
     }
 
     /**
